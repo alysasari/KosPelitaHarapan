848ace136f75b15cd73228741862be06e3c6301c
Rerey <reyvars155@gmail.com>
2025-06-21 21:38:53 +0700

    halaman 404 not found

---
+++ pages/404.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halamannyan tidak ditemukan yaa</title>
+</head>
+<body class="bg-gray-100 flex items-center justify-center h-screen">
+    <div class="text-center">
+        <h1 class="text->404</h1>
+    </div>
+</body>
+</html>
